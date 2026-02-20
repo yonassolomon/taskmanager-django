cdcd3649dff66c7c09a4e1e7cb60a8c63fc47387
yonassolomon <yonaisol21@gmail.com>
2026-02-19 16:13:20 -1000

    CRUD

--- taskmanager/tasks/urls.py
+++ taskmanager/tasks/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('',views.task_list,name='task_list'),
-    path('/create',views.task_create,name='task_create'),
+    path('/create/',views.task_create,name='task_create'),
+    path('<int:task_id>/',views.task_detail,name='task_detail'),
+    path('<int:task_id>/edit',views.task_edit,name='task_edit'),
+    path('<int:task_id>/delete/',views.task_delete,name='task_delete'),
 ]
 
--- taskmanager/tasks/views.py
+++ taskmanager/tasks/views.py
@@ -22,3 +22,30 @@ def task_create(request):
     else:
         form=TaskForm()
     return render(request,'tasks/task_form.html',{'form':form,'title':'New Task'})    
+
+@login_required    
+def task_detail(request,task_id):
+    task=get_object_or_404(Task,id=task_id,user=request.user)
+    return render(request,'tasks/task_detail.html',{'task':task})
+
+@login_required
+def task_edit(request,task_id):
+    task=get_object_or_404(Task,id=task_id,user=request.user)
+    if request.method=='POST':
+        form=TaskForm(request.POST,instance=task)
+        if form.is_valid():
+            form.save()
+            messages.success(request,'✅ Task updated!')
+            return redirect('task_detail',task_id=task.id)
+    else:
+        form=TaskForm(instance=task)
+    return render(request,'tasks/task_form.html',{'form':form,'title':'Edit Task'})    
+
+@login_required
+def task_delete(request, task_id):
+    task = get_object_or_404(Task, id=task_id, user=request.user)
+    if request.method == 'POST':
+        task.delete()
+        messages.success(request, '✅ Task deleted!')
+        return redirect('task_list')
+    return render(request, 'tasks/task_confirm_delete.html', {'task': task})
